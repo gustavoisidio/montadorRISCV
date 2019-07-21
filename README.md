d4a81e285c32ad0d2579ddc03e8661d33e600531
gustavoisidio <gisf@cin.ufpe.br>
2019-07-21 19:06:46 -0300

    README atualizado, add funcionando

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # montadorRISCV
 Montador de instruções para o processador RISC-V
+
+### Instruções Implementadas
+
+#### Tipo R
+- [ ] add
+- [ ] sub
+- [ ] sll
+- [ ] xor
+- [ ] srl
+- [ ] sra
+- [ ] or
+- [ ] and
+
+#### Tipo I
+- [ ] lb
+- [ ] lh
+- [ ] lw
+- [ ] ld
+- [ ] lbu
+- [ ] lhu
+- [ ] lwu
+- [ ] addi
+- [ ] slli
+- [ ] srli
+- [ ] srai
+- [ ] xori
+- [ ] ori
+- [ ] andi
+- [ ] jalr
+
+#### Tipo S
+- [ ] sb
+- [ ] sh
+- [ ] sw
+- [ ] sd
+
+#### Tipo SB
+- [ ] bne
+- [ ] blt
+- [ ] beq
+- [ ] bge
+- [ ] bltu
+- [ ] bgeu
+
+#### Tipo U
+- [ ] lui
+
+#### Tipo UJ
+- [ ] jal
--- montador/exec.hs
+++ montador/exec.hs
@@ -5,8 +5,9 @@ import Data.Char
 
 main = do
     contents <- readFile "instructions.txt"
---    writeFile "girlfriendcaps.txt" ( unlines (jtoUpper ( lines contents ) ) )  
-    writeFile "intructions.mif" ( map ( \x -> case x of ',' ->  ' '; ')' -> ' '; '(' -> ' '; _ -> x) contents)
+    writeFile "instructions.mif" ( unlines (programControll ( lines contents ) 0 ) )  
+    --putStr ( unlines (programControll ( lines contents ) 0 ) )
+--    writeFile "intructions.mif" ( map ( \x -> case x of ',' ->  ' '; ')' -> ' '; '(' -> ' '; _ -> x) contents)
 
 -- Deixa maiuscula a primeira letra de cada linha
 jtoUpper :: [ String ] -> [ String ]
@@ -28,7 +29,30 @@ roundBin count bin = ( drop (( length fatbin ) - ( length bin ) - ( count - ( le
      where fatbin = "000000000" ++ bin
 
 -- String de teste
-instst = "add x5,x3,x0" :: String
+tst = "add x5,x3,x0" :: String
+
+programControll :: [ String ] -> Int -> [ String ]
+programControll _ 12 = [ ] 
+programControll ( firstLine : otherLines ) index = ( instrTitle
+                                                ++ instructionBinary1
+                                                ++ instructionBinary2
+                                                ++ instructionBinary3
+                                                ++ instructionBinary4 ) 
+                                                 : ( programControll otherLines ( index + 4 ) )
+      where instrTitle = ( "--" ++ firstLine ++ "\n" )
+            instructionBinary = instDecod firstLine
+            blockIndex1 = reverse . take 3 $ reverse $ "00" ++ show ( index + 1 )
+            instructionBlock1 = reverse ( take 8 instructionBinary )
+            instructionBinary1 = blockIndex1 ++ ": " ++ take 8 instructionBlock1 ++ ";\n"
+            blockIndex2 = reverse . take 3 $ reverse $ "00" ++ show ( index + 2 )
+            instructionBlock2 = reverse ( take 8 $ drop 8 instructionBinary )
+            instructionBinary2 = blockIndex2 ++ ": " ++ take 8 instructionBlock2 ++ ";\n"
+            blockIndex3 = reverse . take 3 $ reverse $ "00" ++ show ( index + 3 )
+            instructionBlock3 = reverse ( take 8 $ drop 16 instructionBinary )
+            instructionBinary3 = blockIndex3 ++ ": " ++ take 8 instructionBlock3 ++ ";\n"
+            blockIndex4 = reverse . take 3 $ reverse $ "00" ++ show ( index + 4 )
+            instructionBlock4 = reverse ( take 8 $ drop 24 instructionBinary )
+            instructionBinary4 = blockIndex4 ++ ": " ++ take 8 instructionBlock4 ++ ";\n"
 
 -- Recebe uma instrucao e decodifica qual instrucao se trata
 instDecod :: String -> String
@@ -37,14 +61,15 @@ instDecod x | ( x!!0 == 'a' ) && ( x!!1 == 'd' ) && ( x!!2 == 'd' ) = instAdd x
 
 -- Trata a instrucao ADD
 instAdd :: String -> String
-instAdd x = opcode ++ rd ++ funct3 ++ rs1 ++ rs2 ++ funct7 
+instAdd x = reverse ( funct7 ++ rs2 ++ rs1 ++ funct3 ++ rd ++ opcode ) 
     where instCleaned = words $ map ( \x -> case x of ',' ->  ' '; ')' -> ' '; '(' -> ' '; _ -> x) x
-          opcode = "opcode: 0110011 "
-          funct3 = " funct3: 000 "
-          funct7 = " funct7: 0000000"
-          rs1 = "rs1: " ++ ( roundBin 5 ( intToBin $ drop 1 $ instCleaned !! 2 ) ) 
-          rs2 = " rs2: " ++ ( roundBin 5 ( intToBin $ drop 1 $ instCleaned !! 3 ) ) 
-          rd = "rd: " ++ ( roundBin 5 ( intToBin $ drop 1 $ instCleaned !! 1 ) )
-           
+          opcode = "0110011"
+          funct3 = "000"
+          funct7 = "0000000"
+          rs1 = roundBin 5 ( intToBin $ drop 1 $ instCleaned !! 2 ) 
+          rs2 = roundBin 5 ( intToBin $ drop 1 $ instCleaned !! 3 ) 
+          rd = roundBin 5 ( intToBin $ drop 1 $ instCleaned !! 1 )
+
+
 
 
